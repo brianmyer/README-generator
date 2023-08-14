4dc1273fa8b213e0008eb127857b76af2bc33c59
brianmyer <myer.brian@gmail.com>
2023-08-14 15:11:39 -0400

    began creating questions

--- index.mjs
+++ index.mjs
@@ -1,8 +1,60 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
+import fs from 'fs/promises';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = await inquirer 
+.prompt([ {
+    type: 'input',
+    name: 'title',
+    message: "What is the title of your project?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: "Please provide a description of your application",
+  },
+//   {
+//     type: 'input',
+//     name: 'table-of-contents',
+//     message: "Please provide a description of your application",
+//   },
+  {
+    type: 'input',
+    name: 'installation',
+    message: "Please provide any installation instructions",
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: "What is the usage information of this application",
+  },
+//   {
+//     type: 'input',
+//     name: 'license',
+//     message: "Please provide a description of your project",
+//   },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: "Please list any contribution guidelines",
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: "Please provide test instructions for you application",
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: "Please provide your GitHub username",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Please provide your email address",
+  }
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -12,3 +64,37 @@ function init() {}
 
 // Function call to initialize app
 init();
+console.log(questions)
+
+let readmeText = 
+`# ${questions.title}
+
+## Description
+
+${questions.description}
+
+## Table of Contents
+
+## Installation
+
+${questions.installation}
+
+## Usage
+
+${questions.usage}
+
+## License
+
+## Contributing
+
+${questions.contributing}
+
+## Tests
+
+${questions.tests}
+
+## Questions
+
+${questions.username} ${questions.email}`
+
+console.log(readmeText)
